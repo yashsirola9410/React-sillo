a7b4c3127101f3587de1e6f08fe1461400475cfe
yashsirola9410 <yashtechid9410@gmail.com>
2024-12-28 19:18:40 +0530

    custom react kaise kaam karta hai

--- reactstart/src/main.jsx
+++ reactstart/src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// function MyApp(){
+//   return (
+//     <div>
+//       <h1>Custom </h1>
+//     </div>
+//   )
+// }
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherelement = (
+  <a href="https://google.com" target = '_blank'>Visit google</a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {
+    href: 'https://google.com',target: '_blank'
+  },
+  'click me to visit google',
+  anotherelement
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  reactElement
 )
