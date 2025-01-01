d5ed7ff2c7eb173b265572596eb98950f1480c90
yashsirola9410 <yashtechid9410@gmail.com>
2025-01-01 14:32:36 +0530

    counter 2

--- counter/Counter/src/App.jsx
+++ counter/Counter/src/App.jsx
@@ -1,13 +1,28 @@
-
-import './App.css'
+import { useState } from "react"
 
 function App() {
+   const [counter , setCounter] = useState(15)
 
+   const addValue =() =>{
+       setCounter(prevCounter => prevCounter + 1)
+
+   }
+
+   const removeValue =() => {
+      setCounter(counter -1 )
+   }
 
   return (
     <>
-     
-    </>
+     <h1>Yash ka code </h1>
+     <h2> counter value  : {counter}</h2>
+
+     <button onClick ={addValue}>Add value {counter}</button>
+     <br/>
+     <button onClick ={removeValue}> Remove value {counter}</button>
+     <p> footer : {counter}</p>
+     </>
+
   )
 }
 
