9f96f6cada786e8c59f993b0d8d98ee00dd6a772
yashsirola9410 <yashtechid9410@gmail.com>
2025-03-13 17:09:34 +0530

    redux tool kit

--- megaproject/src/App.jsx
+++ megaproject/src/App.jsx
@@ -1,14 +1,39 @@
-
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
 import './App.css'
+import { login, logout } from "./store/authSlice"
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+import { Outlet } from 'react-router-dom'
 
 function App() {
-    console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-  <h1>Blog Project</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO:  <Outlet />
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
-
 export default App
